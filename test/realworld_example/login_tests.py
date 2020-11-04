17fe021eeddecd534a39c1642c54c7e31cd872e3
savva.genchevskiy <savva.genchevskiy@gmail.com>
2020-11-04 12:57:25 +0200

    Github build fix

--- test/realworld_example/login_tests.py
+++ test/realworld_example/login_tests.py
@@ -13,21 +13,21 @@ password = "test.12345678"
 
 # yield_fixture(scope="session") #### To run all test in one browser (Fixture is running ones per session)
 # yield_fixture(scope="function") #### To run all test in separated browsers (Fixture Is Running for every test)
-@pytest.yield_fixture(scope="function")
-def logout_fixture(browser: Browser, request):
-    p: Page = browser.newPage()  # browser.newPage(videosPath="video/")
-    p.context.clearCookies()
-    yield p
-    # Logout
-    # main_page = MainPage(base_url, p)
-    # if p.innerText(main_page.account_button(username).selector) is not None or "":
-    #     settings_page = main_page.open_settings()
-    #     settings_page.logout()
-    screenshot = p.screenshot(path=f"screenshots/{request.node.name}.png", fullPage=True)
-    # video = p.video.path()
-    p.close()
-    allure.attach(screenshot, name=f"{request.node.name}", attachment_type=allure.attachment_type.PNG)
-    # allure.attach.file(f'./{video}', attachment_type=allure.attachment_type.WEBM)
+# @pytest.yield_fixture(scope="function")
+# def logout_fixture(browser: Browser, request):
+#     p: Page = browser.newPage()  # browser.newPage(videosPath="video/")
+#     p.context.clearCookies()
+#     yield p
+#     # Logout
+#     # main_page = MainPage(base_url, p)
+#     # if p.innerText(main_page.account_button(username).selector) is not None or "":
+#     #     settings_page = main_page.open_settings()
+#     #     settings_page.logout()
+#     screenshot = p.screenshot(path=f"screenshots/{request.node.name}.png", fullPage=True)
+#     # video = p.video.path()
+#     p.close()
+#     allure.attach(screenshot, name=f"{request.node.name}", attachment_type=allure.attachment_type.PNG)
+#     # allure.attach.file(f'./{video}', attachment_type=allure.attachment_type.WEBM)
 
 
 @allure.feature("Login")
@@ -45,7 +45,7 @@ def test_should_login_to_system(logout_fixture):
     main_page = login_page.open().login("%s@gmail.com" % username, password)
     assert main_page.account_button(username).innerText() == username
     assert "%s/#/" % base_url in page.url
-    # page.screenshot(path='screenshots/logged_in.png')
+    page.screenshot(path='screenshots/logged_in.png')
 
 
 @allure.feature("Login")
@@ -60,7 +60,7 @@ def test_should_not_login_with_invalid_credentials(logout_fixture):
     assert login_page.email_filed().value() == "%s@gmail.comm" % username
     assert login_page.submit_button().isEnabled()
     assert "%s/#/login" % base_url in page.url
-    # page.screenshot(path='screenshots/invalid_login.png')
+    page.screenshot(path='screenshots/invalid_login.png')
 
 
 @allure.feature("Login")
@@ -76,7 +76,7 @@ def test_should_logout_from_system(logout_fixture):
         .logout()
     assert main_page.login_button().innerText() == "Sign in"
     assert main_page.register_button().innerText() == "Sign up"
-    # page.screenshot(path='screenshots/logged_out.png')
+    page.screenshot(path='screenshots/logged_out.png')
 
 
 
