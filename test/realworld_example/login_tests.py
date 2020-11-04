9234270d11bdde0b38c065013fbcd36c88001449
savva.genchevskiy <savva.genchevskiy@gmail.com>
2020-11-04 13:27:40 +0200

    Github build fix

--- test/realworld_example/login_tests.py
+++ test/realworld_example/login_tests.py
@@ -17,21 +17,21 @@ password = "test.12345678"
 
 # yield_fixture(scope="session") #### To run all test in one browser (Fixture is running ones per session)
 # yield_fixture(scope="function") #### To run all test in separated browsers (Fixture Is Running for every test)
-# @pytest.yield_fixture(scope="function")
-# def logout_fixture(browser: Browser, request):
-#     p: Page = browser.newPage()  # browser.newPage(videosPath="video/")
-#     p.context.clearCookies()
-#     yield p
-#     # Logout
-#     # main_page = MainPage(base_url, p)
-#     # if p.innerText(main_page.account_button(username).selector) is not None or "":
-#     #     settings_page = main_page.open_settings()
-#     #     settings_page.logout()
-#     screenshot = p.screenshot(path=f"screenshots/{request.node.name}.png", fullPage=True)
-#     # video = p.video.path()
-#     p.close()
-#     allure.attach(screenshot, name=f"{request.node.name}", attachment_type=allure.attachment_type.PNG)
-#     # allure.attach.file(f'./{video}', attachment_type=allure.attachment_type.WEBM)
+@pytest.yield_fixture(scope="function")
+def logout_fixture(browser: Browser, request):
+    p: Page = browser.newPage()  # browser.newPage(videosPath="video/")
+    p.context.clearCookies()
+    yield p
+    # Logout
+    # main_page = MainPage(base_url, p)
+    # if p.innerText(main_page.account_button(username).selector) is not None or "":
+    #     settings_page = main_page.open_settings()
+    #     settings_page.logout()
+    screenshot = p.screenshot(path=f"screenshots/{request.node.name}.png", fullPage=True)
+    # video = p.video.path()
+    p.close()
+    allure.attach(screenshot, name=f"{request.node.name}", attachment_type=allure.attachment_type.PNG)
+    # allure.attach.file(f'./{video}', attachment_type=allure.attachment_type.WEBM)
 
 
 @allure.feature("Login")
@@ -43,36 +43,36 @@ password = "test.12345678"
 """)
 @pytest.mark.flaky(reruns=2, reruns_delay=2)
 @pytest.mark.only_browser("chromium")
-def test_should_login_to_system(page: Page):
-    p: Page = page
-    login_page = LoginPage(base_url, page)
+def test_should_login_to_system(logout_fixture):
+    p: Page = logout_fixture
+    login_page = LoginPage(base_url, p)
     main_page = login_page.open().login("%s@gmail.com" % username, password)
     assert main_page.account_button(username).innerText() == username
     assert "%s/#/" % base_url in p.url
-    p.screenshot(path='screenshots/logged_in.png')
+    # p.screenshot(path='screenshots/logged_in.png')
 
 
 @allure.feature("Login")
 @allure.story("Login Flow")
 @allure.title("Login With Invalid Credentials")
 @pytest.mark.only_browser("chromium")
-def test_should_not_login_with_invalid_credentials(page: Page):
-    p: Page = page
+def test_should_not_login_with_invalid_credentials(logout_fixture):
+    p: Page = logout_fixture
     login_page = LoginPage(base_url, p)
     login_page.open().login("%s@gmail.comm" % username, password)
     assert login_page.error_message().innerText() == "email or password is invalid"
     assert login_page.email_filed().value() == "%s@gmail.comm" % username
     assert login_page.submit_button().isEnabled()
     assert "%s/#/login" % base_url in p.url
-    p.screenshot(path='screenshots/invalid_login.png')
+    # p.screenshot(path='screenshots/invalid_login.png')
 
 
 @allure.feature("Login")
 @allure.story("Logout")
 @allure.title("Logout from System")
 @pytest.mark.only_browser("chromium")
-def test_should_logout_from_system(page: Page):
-    p: Page = page
+def test_should_logout_from_system(logout_fixture):
+    p: Page = logout_fixture
     login_page = LoginPage(base_url, p)
     main_page = login_page.open() \
         .login("%s@gmail.com" % username, password) \
@@ -80,7 +80,7 @@ def test_should_logout_from_system(page: Page):
         .logout()
     assert main_page.login_button().innerText() == "Sign in"
     assert main_page.register_button().innerText() == "Sign up"
-    p.screenshot(path='screenshots/logged_out.png')
+    # p.screenshot(path='screenshots/logged_out.png')
 
 
 
